7484297a809a0e5bf456a7e4470e3e6a95bdde03
Hendi Santika <hendisantika@gmail.com>
2019-01-10 05:41:36 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/healthcheckprometheus/HealthcheckPrometheusApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.healthcheckprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcheckPrometheusApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/healthcheckprometheus/HealthcheckPrometheusApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.healthcheckprometheus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthcheckPrometheusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
