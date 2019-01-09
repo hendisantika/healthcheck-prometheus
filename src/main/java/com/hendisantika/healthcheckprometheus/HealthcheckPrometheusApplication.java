a781202205512837f8346069101b0344145a79a8
Hendi Santika <hendisantika@gmail.com>
2019-01-10 05:49:21 +0700

    #5 - Modify Main App

--- src/main/java/com/hendisantika/healthcheckprometheus/HealthcheckPrometheusApplication.java
+++ src/main/java/com/hendisantika/healthcheckprometheus/HealthcheckPrometheusApplication.java
@@ -1,13 +1,18 @@
 package com.hendisantika.healthcheckprometheus;
 
+import com.github.strengthened.prometheus.healthchecks.HealthChecksCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthcheckPrometheusApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(HealthcheckPrometheusApplication.class, args);
+        HealthChecksCollector healthchecksMetrics = HealthChecksCollector.Builder.of().setGaugeMetricName("naruto_health_check").build();
+        healthchecksMetrics.addHealthCheck("database", new DbHealthCheck());
+
+        new HealthCheckExporter(healthchecksMetrics).export();
     }
 
 }
